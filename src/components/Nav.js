748623a359f9e6358e1520d0c0055ad7c0c4b22e
Wesley Jiang <wjiang917@gmail.com>
2023-11-20 17:43:58 -0600

    Create some files

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return(
+        <nav>
+            <ul>
+                <li>Hi</li>
+                <li>Hello</li>
+                <li>Hola</li>
+            </ul>
+        </nav>
+    );
+}
